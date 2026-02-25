8801c1db6ae0fb0f939660aff5dc06043039b515
xamidovscode <xamidovcoder@gmail.com>
2026-02-25 20:17:26 +0500

    some changes

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from apps.common.models import TimeStamp, DeleteMixin
+from apps.users.managers import UserManager
+
+
+class User(AbstractUser, TimeStamp, DeleteMixin):
+
+    first_name = None
+    last_name = None
+    email = None
+
+    full_name = models.CharField(
+        max_length=255, verbose_name='Full Name'
+    )
+
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+
--- config/settings/base.py
+++ config/settings/base.py
@@ -16,6 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.common',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -29,6 +31,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+AUTH_USER_MODEL = "users.User"
 
 TEMPLATES = [
     {
