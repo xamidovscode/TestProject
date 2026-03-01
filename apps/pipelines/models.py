2c202d375e0f6c82e60f90786d73f21adfebcfcd
xamidovscode <xamidovcoder@gmail.com>
2026-03-01 16:37:25 +0500

    some changes

--- apps/pipelines/models.py
+++ apps/pipelines/models.py
@@ -17,12 +17,29 @@ class Pipeline(TimeStamp, DeleteMixin):
         verbose_name='Background',
     )
 
-
     class Meta:
         db_table = 'pipelines'
         verbose_name = 'Pipeline'
         verbose_name_plural = 'Pipelines'
 
 
+class Status(TimeStamp, DeleteMixin):
 
+    class StatusChoices(models.IntegerChoices):
+        NEW = (0, 'New')
+        SUCCESS = (1, 'Success')
+        DONE = (3, 'Done')
+
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Status name',
+    )
+    order = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Order',
+    )
+    deletable = models.BooleanField(
+        default=True,
+        verbose_name='Deleteable',
+    )
 
