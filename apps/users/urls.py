00ae64223048c2fc67c7e7a36607002da1b2e1be
xamidovscode <xamidovcoder@gmail.com>
2026-04-02 17:06:49 +0500

    register and verify

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,9 +4,9 @@ from rest_framework.views import APIView
 from .import views
 
 urlpatterns = [
-    path('auth/register/',views.RegisterAPIView.as_view(),name='register'),
-    path('auth/verify-email/',views.ValidateEmailAPIView.as_view(),name='verify_email'),
-    path('auth/resend-code/',views.ResendCodeAPIView.as_view(),name='resend_code'),
-    path('auth/login/',views.LoginAPIView.as_view(),name='login'),
+    path('register/',views.RegisterAPIView.as_view(),name='register'),
+    path('verify-email/',views.ValidateEmailAPIView.as_view(),name='verify_email'),
+    path('resend-code/',views.ResendCodeAPIView.as_view(),name='resend_code'),
+    path('login/',views.LoginAPIView.as_view(),name='login'),
 ]
 
