4d385c94a2216ab0dda7c810839bd99ca1910052
Akmal <akmalv570@gmail.com>
2026-04-18 14:26:46 +0500

    commit

--- config/urls/base.py
+++ config/urls/base.py
@@ -6,10 +6,11 @@ from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 urlpatterns = [
     path('admin-ad/', admin.site.urls),
     path('api/v1/', include('apps.api_v1')),
-    path("schema-sc/", SpectacularAPIView.as_view(), name="api-schema"),
-    path("docs-do/", SpectacularSwaggerView.as_view(url_name="api-schema")),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+    path("schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path("docs/", SpectacularSwaggerView.as_view(url_name="api-schema")),
+
