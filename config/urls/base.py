5509891a59a9e0124b594a25c137e9b43360b473
Akmal <akmalv570@gmail.com>
2026-04-18 14:19:35 +0500

    commit

--- config/urls/base.py
+++ config/urls/base.py
@@ -4,10 +4,10 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
-    path('admin-qw/', admin.site.urls),
+    path('admin-ad/', admin.site.urls),
     path('api/v1/', include('apps.api_v1')),
-    path("schema-qw/", SpectacularAPIView.as_view(), name="api-schema"),
-    path("docs-qw/", SpectacularSwaggerView.as_view(url_name="api-schema")),
+    path("schema-sc/", SpectacularAPIView.as_view(), name="api-schema"),
+    path("docs-do/", SpectacularSwaggerView.as_view(url_name="api-schema")),
 ]
 
 if settings.DEBUG:
